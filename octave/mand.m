5788cef3ae7779193f4cfeefe615c4199e3a3604
Luan Nico <luanpotter27@gmail.com>
2017-04-23 13:10:50 -0300

    Adding box cound and comments

--- octave/mand.m
+++ octave/mand.m
@@ -1,38 +1,81 @@
 function mand()
 
-  ITER = 200
-  details = 128
+  ITER = 200 # Number of consecutive iterations that are necessary to determine that a constant growth means explosion
+  details = 128 # Size of the result image, in pixels (always square, details x details) ; beware : should be a power of 2
 
-  function M = mandf(c)
-    z = c;
-    max = 0;
-    lastMax = 0;
-    niter = 100*ITER - 1;
+  function M = mandf(c) # returns whether the complex c belongs to the mand set
+    z = c; # current point
+    max = 0; # current max z
+    lastMax = 0; # previous max z
+    niter = 100*ITER - 1; # total, number of iterations, if no growth is detected in this range, assume limited
     for t = 0:niter
       a = abs(z);
-      if (isinf(a))
+      if (isinf(a)) # exploded! for sure
         M = 0;
 	return
       endif
       if (a > max)
         max = a;
 	lastMax = t;
-      elseif (t - lastMax > ITER)
+      elseif (t - lastMax > ITER) # it's growing non stop for ITER iterations, must explode
         M = 1;
 	return
       endif
-      z = z*z + c;
+      z = z*z + c; # next in the sequence
     endfor
-    M = 0;
+    M = 0; # didn't explode in niter iterations
   endfunction
 
-  xc = double(0)
+  function D = dim(frac) # calculates the dimension of the frac with the box count method
+    n = log2(details);
+    xs = zeros(n);
+    ys = zeros(n);
+    for i=1:n
+      r = pow2(i - 1);
+      squares = countSquares(frac, r);
+      xs(i) = log(r);
+      ys(i) = log(squares);
+    endfor
+    mmq = (ys\xs)
+    D = mmq(1);
+  endfunction
+
+  function total = countSquares(fractal, r)
+    total = 0;
+    size = details / r;
+    ci = 0;
+    cj = 0;
+    while ci < details
+     while cj < details
+        if countSubSquare(picture, ci, cj, size) == 1
+          total += 1;
+        endif
+        ci += size;
+        cj += size;
+      endwhile
+    endwhile
+  endfunction
+
+  function B = countSubSquare(fractal, i, j, s)
+    for dx = 1:(s - 1)
+      for dy = 1:(s - 1)
+        if fractal(i + dx, j + dy) == 1
+          B = 1;
+          return
+        endif
+      endfor
+    endfor
+    B = 0;
+  endfunction
+
+  xc = double(0) # x,y coord of the center of the mand set (the center of the picture will be this point of the mand set
   yc = double(0)
-  size = double(5)
+  size = double(5) # size of the mand we will build; the border of the image will have coord 5 units in the mand set
+  # e.g., if (x,y) = (0,0) and size = 1, we are going to print the mand set, from x = -.5 to +.5, y = -.5 to +.5
 
-  imgu = i;
+  imgu = i; # because I used i in the for and got all mixed up -.-
 
-  picture = ones(details, details);
+  picture = ones(details, details); # fill with ones! (we well zero if the dot does not belong)
   for i = 0:(details - 1)
     for j = 0:(details - 1)
       x0 = xc - size / 2 + size * i / details;
@@ -43,5 +86,7 @@ function mand()
     endfor
   endfor
 
-  imwrite(picture, 'test.png')
+  d = dim(picture)
+
+  imwrite(picture, 'mand.png');
 endfunction
