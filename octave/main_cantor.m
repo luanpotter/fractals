c31690d0bdb80f628c8186b49f9e3d21fa6d05a9
Luan Nico <luanpotter27@gmail.com>
2017-05-14 23:03:49 -0300

    optimization

--- octave/main_cantor.m
+++ octave/main_cantor.m
@@ -1,18 +1,29 @@
 function main_cantor()
 
+  H = 100
+
+  function nb = next_block_list(block_list)
+    nb = [];
+    for block = (block_list*3)
+      nb = [nb (block - 4) (block + 2)];
+    endfor
+  endfunction
+
   function picture = generate(size, it)
-    picture = zeros(1, size);
+    picture = zeros(H, size);
+    block_list = [2];
     blocks = 1;
     for i = 1:it
       blocks = blocks * 3;
       blockSize = size/blocks;
-      for pair = 2:2:blocks
-        picture(1, ((pair - 1)*blockSize+1):pair*blockSize) = 1;
+      for block = block_list
+        picture(1:H, (block - 1)*blockSize:block*blockSize) = 1;
       endfor
+      block_list = next_block_list(block_list);
     endfor
   endfunction
 
-  picture = generate(3^6, 10);
+  picture = generate(3^11, 10);
   imwrite(picture, 'results/cantor.png');
 endfunction
 
